931c4de9cd841942ba21ff7a72be0eb38dc061ed
Aman Lalpuria <aman.sharma163@gmail.com>
2025-04-24 22:51:40 +0530

    Initial test

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = var.aws_region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "sipstr-terraform-state-bucket"
+    key            = "env/app/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
