c67f18ad0048c8c1b2e7be87b3f1fa88c0bb6acb
Aman Lalpuria <aman.sharma163@gmail.com>
2025-04-24 18:34:29 +0530

    Initial test

--- infra/security.tf
+++ infra/security.tf
@@ -23,22 +23,3 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-# resource "aws_security_group" "rds_sg" {
-#   name        = "rds_sg"
-#   description = "Allow PostgreSQL access"
-#
-#   ingress {
-#     from_port       = 5432
-#     to_port         = 5432
-#     protocol        = "tcp"
-#     security_groups = [aws_security_group.ec2_sg.id]
-#   }
-#
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
